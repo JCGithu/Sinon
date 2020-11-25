da869ecbfac32fd820379f9657fad7a941f99c80
Jack Gracie <jack.c.gracie@gmail.com>
2020-11-25 13:21:14 +0000

    Rewrite: now only scraping .m3u8 and with JS

--- scripts/downloader/scrapers/down_parliament.js
+++ scripts/downloader/scrapers/down_parliament.js
@@ -1,55 +1,29 @@
+const axios = require('axios');
+const runningAlert = require('../../alerts/runningAlert');
+const successAlert = require('../../alerts/successAlert');
+const errorAlert = require('../../alerts/errorAlert');
+const { webCheck } = require('../../utilities/utils');
+
 function down_parliament(data) {
-  Swal.fire({
-    icon: 'success',
-    title: 'Video found!',
-    text: 'Nice one, get .m3u8 url or download',
-    input: 'select',
-    inputOptions: {
-      live: 'Grab .m3u8 Code',
-      normal: 'Download Past Stream',
-    },
-    inputPlaceholder: 'Select Quality',
-    confirmButtonText: 'Run!',
-    showLoaderOnConfirm: true,
-    backdrop: swalColour.loading,
-    target: document.getElementById('swalframe'),
-    preConfirm: (dlquality) => {
-      let quals = ['normal', 'live'];
+  webCheck(data).then((checked) => {
+    if (checked){
       runningAlert();
-      for (let i = 0; i < quals.length; i++) {
-        if (dlquality == 'normal') {
-          execFile(
-            versionInfo.ExtractorSet,
-            [
-              data.URL,
-              data.path,
-              order,
-              data.proxy,
-              versionInfo.ffmpegPath,
-              data.instaUse,
-              data.instaPass,
-            ],
-            extractorOptions,
-            (error, stdout) => {
-              if (error) {
-                errorAlert(error, 'download', '', swalColour, '');
-              } else {
-                var livestreamurl = stdout.replace('\\r/', '/');
-                successAlert(quals[i], livestreamurl, swalColour);
-              }
-            }
-          );
-        } else {
-          //Python to change to JS
-/*           if 'parliamentlive.tv' in parsedURL:
-          feednumber = re.findall('x\/(.*?.*)', str(parsedURL))
-          feednumbertwo = str(feednumber).replace('[','').replace(']','').replace("'","")
-          anotherurl = 'https://ukparliament.cdn.eurovisioncdn.net/live/' + str(feednumbertwo) + '/live.isml/live-audio_track_0_eng=64000-video=1300000.m3u8'
-          print (anotherurl) */
-        }
-      }
-    },
-  });
+      var regex = /[0-9A-Za-z]+-[0-9A-Za-z]+-[0-9A-Za-z]+-[0-9A-Za-z]+-[0-9A-Za-z]+/g;
+      var code = data.URL.match(regex);
+      data.URL = `http://videoplayback.parliamentlive.tv/Player/Live/${code}`
+      axios
+        .get(data.URL)
+        .then(async (response, error) => {
+          if (error){
+            errorAlert(error, '', '', swalColour, "");
+          } else {
+            var regex = /[0-9A-Za-z\/\.\-\:]+.m3u8/g;
+            var livestreamURL = response.data.match(regex);
+            successAlert('live', livestreamURL[0], swalColour);
+          }
+        })
+    }
+  })
 }
 
 module.exports = down_parliament;
