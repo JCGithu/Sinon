274acee28d9681c1f1ae8a6a403d5469ec1d3040
Jack Gracie <jack.c.gracie@gmail.com>
2020-11-26 22:15:59 +0000

    removed periscope

--- engine/dllive.py
+++ engine/dllive.py
@@ -21,32 +21,18 @@ def download(parsedURL):
 		if d['status'] == 'finished':
 			print ('Live URL extract success')
 
-		if 'pscp.tv' in parsedURL:
-			ydl_opts = {
-				'forceurl': True,
-				'format': 'lhlsweb/lhls/hls/best',
-				'forceurl': True,
-				'quiet': True,
-			}
+	ydl_opts = {
+		'format': '95/300/94/93',
+		'forceurl': True,
+		'quiet': True,
+	}
 
-			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-				r = ydl.extract_info(parsedURL, False)
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		r = ydl.extract_info(parsedURL, False)
+		rtwo = (r['formats'])
+		if '/96/' in str(rtwo):
+				print (r['formats'][-2]['url'])
+		else:
 				print (r['formats'][-1]['url'])
 
-			sys.stdout.flush()
-		else:
-			ydl_opts = {
-				'format': '95/300/94/93',
-				'forceurl': True,
-				'quiet': True,
-			}
-
-			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-				r = ydl.extract_info(parsedURL, False)
-				rtwo = (r['formats'])
-				if '/96/' in str(rtwo):
-						print (r['formats'][-2]['url'])
-				else:
-						print (r['formats'][-1]['url'])
-
-			sys.stdout.flush()
+	sys.stdout.flush()
