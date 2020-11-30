82309a65da233e05a383302238daedcdb9619c76
Jack Gracie <jack.c.gracie@gmail.com>
2020-11-30 11:22:00 +0000

    moved to plainExec

--- scripts/downloader/scrapers/down_skip.js
+++ scripts/downloader/scrapers/down_skip.js
@@ -1,7 +1,10 @@
 const runningAlert = require('../../alerts/runningAlert');
 const successAlert = require('../../alerts/successAlert');
 const errorAlert = require('../../alerts/errorAlert');
+
 const { execFile } = require('child_process');
+const plainExec = require('../execs/plainExec');
+const axios = require('axios');
 
 function down_skip(data, extractorOptions) {
   Swal.fire({
@@ -13,42 +16,21 @@ function down_skip(data, extractorOptions) {
     backdrop: swalColour.loading,
     preConfirm: () => {
       runningAlert();
-      if (data.URL.indexOf('itv.com/hub') >= 0) {
-        extractorOptions.maxBuffer = 1024 * 30720;
-      }
-      data.options = 'normal';
-      execFile(
-        versionInfo.ExtractorSet,
-        [
-          data.URL,
-          data.path,
-          data.options,
-          data.proxy,
-          versionInfo.ffmpegPath,
-          data.instaUse,
-          data.instaPass,
-        ],
-        extractorOptions,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.log(stdout, stderr)
-            console.log('Generic downloader error, details:');
-            errorAlert(error, 'download', '', swalColour);
-          } else {
-            if (data.URL.indexOf('twitter.com') >= 0) {
-              successAlert(
-                '',
-                'Please note: Twitter videos are named after the username! Some tweets are just too long for titles.',
-                swalColour
-              );
-            } else {
-              successAlert('', '', swalColour);
-            }
-          }
-        }
-      );
+      dataParse(data).then((data)=>{
+        data.options = 'normal';
+        plainExec(data, extractorOptions);
+      })
     },
   });
+}
+
+function dataParse(data){
+  return new Promise((res) =>{
+    if (data.URL.indexOf('itv.com/hub') >= 0) {
+      extractorOptions.maxBuffer = 1024 * 30720;
+    }
+    res(data)
+  })
 }
 
 module.exports = down_skip;
