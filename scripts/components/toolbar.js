b5d8f159db2c33780e319a6c7c79caea2bae1627
Jack Gracie <jack.c.gracie@gmail.com>
2020-12-10 21:24:16 +0000

    v1.3.2

--- scripts/components/toolbar.js
+++ scripts/components/toolbar.js
@@ -14,7 +14,7 @@ const toolbar = {
 					<svg id="optionsIcon" viewBox="0 0 640 512">
 						<use xlink:href="../imgs/tools.svg#optionsBox"></use>
 					</svg>
-					<a style="font-size: 8pt;" class="grow-on-hover" id='version'>Version <span id='ver'>1.3.1</span></a>
+					<a style="font-size: 8pt;" class="grow-on-hover" id='version'>Version <span id='ver'>1.3.2</span></a>
 				</div>
 			</header>
     `;
