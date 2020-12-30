b2e587eb867746e3724c6ad2baa4d4fdb2a5ac11
jcgithu <jack.c.gracie@gmail.com>
2020-12-30 15:33:48 +0000

    patched .mp4 download

--- scripts/downloader/execs/fileExec.js
+++ scripts/downloader/execs/fileExec.js
@@ -6,43 +6,51 @@ const errorAlert = require('../../alerts/errorAlert');
 const { progressBar } = require('../../utilities/utils');
 
 const { execFile } = require('child_process');
-const { promisify } = require('util');
-const stream = require('stream');
-const got = require('got');
+const fetch = require('node-fetch');
 
-function fileExec(data){
+async function fileExec(data) {
   runningAlert();
-  if (data.URL.indexOf('.mp4') >= 0 && data.URL.indexOf('.m3u8') == 0 || data.URL.indexOf('.mp3') >= 0 && data.URL.indexOf('.m3u8') == 0) {
+  console.log('file exec');
+  if (
+    (data.URL.indexOf('.mp4') >= 0 && data.URL.indexOf('.m3u8') < 0) ||
+    (data.URL.indexOf('.mp3') >= 0 && data.URL.indexOf('.m3u8') < 0)
+  ) {
     console.log(data.URL);
-    let fileName = /\/[^\/]+$/.exec(data.URL);
-    data.path = data.path.concat(fileName.toString().replace('/', '\\'));
+    let filetype = '.mp4';
+    if (data.URL.indexOf('.mp3') >= 0) {
+      filetype = '.mp3';
+    }
+    let fileName = `${data.hostname}`;
+    const response = await fetch(data.URL);
+    const buffer = await response.buffer();
 
-    const pipeline = promisify(stream.pipeline);
+    async function downloader() {
+      fs.writeFile(`${data.path}\\${fileName}${filetype}`, buffer, () => {});
+    }
 
-    (async () => {
-      try {
-        await pipeline(got.stream(data.URL), fs.createWriteStream(data.path));
-        successAlert();
-      } catch (error) {
+    downloader()
+      .catch((error) => {
         errorAlert(error, 'download', '');
-      }
-    })();
+      })
+      .then(() => {
+        successAlert();
+      });
   } else {
     outputFormat = data.path + '/' + data.hostname + '.mp4';
     ffmpeg(data.URL)
-    .format('mp4')
-    .save(outputFormat)
-    .on('error', (err, stdout, stderr) => {
-      err = err + stdout + stderr;
-      errorAlert(err, 'download', '');
-    })
-    .on('progress', (progress) => {
-      progressBar(progress, '');
-    })
-    .on('end', () => {
-      successAlert();
-    })
-    .run();
+      .format('mp4')
+      .save(outputFormat)
+      .on('error', (err, stdout, stderr) => {
+        err = err + stdout + stderr;
+        errorAlert(err, 'download', '');
+      })
+      .on('progress', (progress) => {
+        progressBar(progress, '');
+      })
+      .on('end', () => {
+        successAlert();
+      })
+      .run();
   }
 }
 
