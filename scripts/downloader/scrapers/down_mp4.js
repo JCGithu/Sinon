e32c6582e48e1d53bb518f9f9e445065f26e1f0b
jcgithu <jack.c.gracie@gmail.com>
2021-02-18 13:41:55 +0000

    patched mp4 downloading

--- scripts/downloader/scrapers/down_mp4.js
+++ scripts/downloader/scrapers/down_mp4.js
@@ -13,55 +13,61 @@ function down_mp4(data, extractorOptions) {
     backdrop: swalColour.loading,
     preConfirm: () => {
       runningAlert();
-      console.log(data)
-      scraping(data).then((data) =>{
+      console.log(data);
+      scraping(data).then((data) => {
         data.options = 'normal';
-        plainExec(data, extractorOptions)
-      })
+        plainExec(data, extractorOptions);
+      });
     },
   });
 }
 
-async function urlFix (url){
-  let newURL = url.replace(/\\\/|\/\\|\/\/|\\\\/g, '/').replace(':/', '://').replace(':///', '://');
-  return newURL
+async function urlFix(url) {
+  let newURL = url
+    .replace(/\\\/|\/\\|\/\/|\\\\/g, '/')
+    .replace(':/', '://')
+    .replace(':///', '://');
+  return newURL;
 }
 
-function scraping(data){
-  return new Promise ((resolve) => {
+function scraping(data) {
+  return new Promise((resolve) => {
     proxyAxios(data).then((axiosOpts) => {
       axios
-      .get(data.URL, axiosOpts)
-      .then(async (response, error) => {
-        var regex = /[0-9A-Za-z\/\.\-\:\_\\\-]+.\.mp4/g
-        var videoURLS = response.data.match(regex);
-        var tempURL = await urlFix(videoURLS[0])
-        if (data.URL.includes('metro.co.uk')){
-          tempURL = tempURL.replace("640x360", "1024x576").replace("960x540", "1024x576").replace("480x270", "1024x576")
-        }
-        data.URL = tempURL
-        resolve(data);
-    }).catch((error)=>{
-      errorAlert(error, '', '');
-    })
-    })
-  })
+        .get(data.URL, axiosOpts)
+        .then(async (response, error) => {
+          var regex = /[0-9A-Za-z\/\.\-\:\_\\\-]+.\.mp4/g;
+          var videoURLS = response.data.match(regex);
+          var tempURL = await urlFix(videoURLS[0]);
+          if (data.URL.includes('metro.co.uk')) {
+            tempURL = tempURL
+              .replace('640x360', '1024x576')
+              .replace('960x540', '1024x576')
+              .replace('480x270', '1024x576');
+          }
+          data.URL = tempURL;
+          resolve(data);
+        })
+        .catch((error) => {
+          errorAlert(error, '', '');
+        });
+    });
+  });
 }
 
-function proxyAxios(data){
-  return new Promise((res)=>{
-    let axiosOpts = {}
-    if (data.proxyUse == true){
-      let proxyHost = data.proxy.split(':')
-      console.log(proxyHost)
+function proxyAxios(data) {
+  return new Promise((res) => {
+    let axiosOpts = {};
+    if (data.proxyUse == true) {
+      let proxyHost = data.proxy.split(':');
+      console.log(proxyHost);
       axiosOpts.proxy = {
-        host: 'https://' + proxyHost[0], 
+        host: 'https://' + proxyHost[0],
         port: proxyHost[1],
-      }
-      
+      };
     }
-    res(axiosOpts)
-  })
+    res(axiosOpts);
+  });
 }
 
 module.exports = down_mp4;
