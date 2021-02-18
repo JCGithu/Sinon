d293cc713c6d584edabd54d129698289a8db6cc0
jcgithu <jack.c.gracie@gmail.com>
2021-02-18 13:46:56 +0000

    WIP scraping rewrite

--- scripts/downloader/scrapers/down_scrape.js
+++ scripts/downloader/scrapers/down_scrape.js
@@ -1,9 +1,13 @@
+const successAlert = require('../../alerts/successAlert');
 const runningAlert = require('../../alerts/runningAlert');
 const errorAlert = require('../../alerts/errorAlert');
+
 const plainExec = require('../execs/plainExec');
+const { execFile } = require('child_process');
 
 const HttpsProxyAgent = require('https-proxy-agent');
 const fetch = require('node-fetch');
+const fileExec = require('../execs/fileExec');
 
 function down_scrape(data, extractorOptions) {
   Swal.fire({
@@ -15,55 +19,76 @@ function down_scrape(data, extractorOptions) {
     backdrop: swalColour.loading,
     preConfirm: () => {
       runningAlert();
-      console.log(data)
-      scraping(data).then((data) =>{
-        if (data.options == 'normal'){
-          plainExec(data, extractorOptions)
-        }
-      })
+      scraping(data, extractorOptions).then((data) => {
+        fileExec(data);
+      });
     },
   });
 }
 
-async function urlFix (url){
-  let newURL = url.replace(/\\\/|\/\\|\/\/|\\\\/g, '/').replace(':/', '://').replace(':///', '://');
-  return newURL
+async function urlFix(url) {
+  let newURL = url
+    .replace(/\\\/|\/\\|\/\/|\\\\/g, '/')
+    .replace(':/', '://')
+    .replace(':///', '://');
+  return newURL;
 }
 
-function scraping(data){
-  return new Promise ((resolve) => {
-    if (data.proxyUse == true){
-      if (data.proxy !== ''){
-        fetch(data.URL, {agent: new HttpsProxyAgent('http://' + data.proxy)})
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          errorAlert(err, 'download', '');
-          console.log(err);
-        });
+function scraping(data, extractorOptions) {
+  return new Promise((resolve) => {
+    if (data.proxyUse == true) {
+      if (data.proxy !== '') {
+        fetch(data.URL, { agent: new HttpsProxyAgent('http://' + data.proxy) })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            errorAlert(err, 'download', '');
+            console.log(err);
+          });
       } else {
-        errorAlert('', 'basic', "This site needs a proxy and I can't find one! Maybe wait a second as I may still be looking for one");
-      }
-    } else {
-      if (data.hostname == 'abc7' || data.hostname == 'abc13'){
-        data.options = 'print';
-        execFile(
-          versionInfo.ExtractorSet,
-          [data.URL, data.path, data.options, data.proxy, versionInfo.ffmpegPath, data.instaUse, data.instaPass],
-          extractorOptions,
-          (error, stdout) => {
-            if (error) {
-              errorAlert(error, 'download', '');
-            } else {
-              console.log(stdout);
-              successAlert(quals[i], stdout);
-            }
-          }
+        errorAlert(
+          '',
+          'basic',
+          "This site needs a proxy and I can't find one! Maybe wait a second as I may still be looking for one"
         );
       }
+    } else {
+      data.options = 'print';
+      execFile(
+        versionInfo.ExtractorSet,
+        [data.URL, data.path, data.options, data.proxy, versionInfo.ffmpegPath, data.instaUse, data.instaPass],
+        extractorOptions,
+        (error, stdout) => {
+          if (error) {
+            errorAlert(error, 'download', '');
+          } else {
+            let sources = stdout
+              .replace('[', '')
+              .replace(']', '')
+              .split(/(?<=}),/g);
+            console.log(sources);
+            let value = 0;
+            for (let i = 0; i <= sources.length; i++) {
+              console.log(sources[i]);
+              let lastChars = sources[i].slice(-2);
+              let source = sources[i];
+              if (lastChars !== '}}') {
+                source = sources[i] + '}';
+              }
+              let videoData = JSON.parse(source);
+              if (videoData.height > value) {
+                data.URL = videoData.url;
+              }
+              if (i == sources.length) {
+                resolve(data);
+              }
+            }
+          }
+        }
+      );
     }
-  })
+  });
 }
 
 module.exports = down_scrape;
