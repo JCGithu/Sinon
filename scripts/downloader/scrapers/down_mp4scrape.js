361ae4dd986b3301524510b0d25e16490279f81d
jcgithu <jack.c.gracie@gmail.com>
2021-02-18 11:05:24 +0000

    more ts patching

--- scripts/downloader/scrapers/down_mp4scrape.js
+++ scripts/downloader/scrapers/down_mp4scrape.js
@@ -1,14 +1,131 @@
-<<<<<<< Updated upstream:scripts/downloader/scrapers/down_mp4scrape.js
-const runningAlert = require('../../alerts/runningAlert');
-const successAlert = require('../../alerts/successAlert');
-const errorAlert = require('../../alerts/errorAlert');
-
-const { execFile } = require('child_process');
-const inputDetails = require('../inputDetails');
-const plainExec = require('../execs/plainExec');
-const axios = require('axios');
-
-function down_mp4scrape(data, extractorOptions) {
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+var __generator =
+  (this && this.__generator) ||
+  function (thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function () {
+          if (t[0] & 1) throw t[1];
+          return t[1];
+        },
+        trys: [],
+        ops: [],
+      },
+      f,
+      y,
+      t,
+      g;
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === 'function' &&
+        (g[Symbol.iterator] = function () {
+          return this;
+        }),
+      g
+    );
+    function verb(n) {
+      return function (v) {
+        return step([n, v]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError('Generator is already executing.');
+      while (_)
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t = op[0] & 2 ? y['return'] : op[0] ? y['throw'] || ((t = y['return']) && t.call(y), 0) : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op;
+              break;
+            case 4:
+              _.label++;
+              return { value: op[1], done: false };
+            case 5:
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
+            case 7:
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
+            default:
+              if (!((t = _.trys), (t = t.length > 0 && t[t.length - 1])) && (op[0] === 6 || op[0] === 2)) {
+                _ = 0;
+                continue;
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1];
+                break;
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1];
+                t = op;
+                break;
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2];
+                _.ops.push(op);
+                break;
+              }
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
+          }
+          op = body.call(thisArg, _);
+        } catch (e) {
+          op = [6, e];
+          y = 0;
+        } finally {
+          f = t = 0;
+        }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
+    }
+  };
+var runningAlert = require('../../alerts/runningAlert');
+var errorAlert = require('../../alerts/errorAlert');
+var plainExec = require('../execs/plainExec');
+var axios = require('axios');
+function down_mp4(data, extractorOptions) {
   Swal.fire({
     icon: 'success',
     title: 'Video found!',
@@ -16,177 +133,77 @@ function down_mp4scrape(data, extractorOptions) {
     confirmButtonText: 'Download',
     showLoaderOnConfirm: true,
     backdrop: swalColour.loading,
-    preConfirm: () => {
+    preConfirm: function () {
       runningAlert();
-      console.log(data)
-      scraping(data).then((data) =>{
+      console.log(data);
+      scraping(data).then(function (data) {
         data.options = 'normal';
-        plainExec(data, extractorOptions)
-      })
+        plainExec(data, extractorOptions);
+      });
     },
   });
-=======
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var runningAlert = require('../../alerts/runningAlert');
-var errorAlert = require('../../alerts/errorAlert');
-var plainExec = require('../execs/plainExec');
-var axios = require('axios');
-function down_mp4(data, extractorOptions) {
-    Swal.fire({
-        icon: 'success',
-        title: 'Video found!',
-        showCancelButton: true,
-        confirmButtonText: 'Download',
-        showLoaderOnConfirm: true,
-        backdrop: swalColour.loading,
-        preConfirm: function () {
-            runningAlert();
-            console.log(data);
-            scraping(data).then(function (data) {
-                data.options = 'normal';
-                plainExec(data, extractorOptions);
-            });
-        }
-    });
->>>>>>> Stashed changes:scripts/downloader/scrapers/down_mp4.js
 }
 function urlFix(url) {
-    return __awaiter(this, void 0, void 0, function () {
-        var newURL;
-        return __generator(this, function (_a) {
-            newURL = url.replace(/\\\/|\/\\|\/\/|\\\\/g, '/').replace(':/', '://').replace(':///', '://');
-            return [2 /*return*/, newURL];
-        });
+  return __awaiter(this, void 0, void 0, function () {
+    var newURL;
+    return __generator(this, function (_a) {
+      newURL = url
+        .replace(/\\\/|\/\\|\/\/|\\\\/g, '/')
+        .replace(':/', '://')
+        .replace(':///', '://');
+      return [2 /*return*/, newURL];
     });
+  });
 }
-<<<<<<< Updated upstream:scripts/downloader/scrapers/down_mp4scrape.js
-
-function scraping(data){
-  return new Promise ((resolve) => {
-    console.log('ran scraping')
-    proxyAxios(data).then((axiosOpts) => {
-      axios
-      .get(data.URL, axiosOpts)
-      .then(async (response, error) => {
-        console.log('huzzah for axios')
-        if (error){
-          errorAlert(error, '', '');
-        } else {
-          var regex = /[0-9A-Za-z\/\.\-\:\_\\\-]+.\.mp4/g
-          var videoURLS = response.data.match(regex);
-          var tempURL = await urlFix(videoURLS[0])
-          if (data.URL.includes('metro.co.uk')){
-            tempURL = tempURL.replace("640x360", "1024x576").replace("960x540", "1024x576").replace("480x270", "1024x576")
-          }
-          data.URL = tempURL
-          resolve(data);
-        }
-    }).catch((error)=>{
-      console.log(axiosOpts)
-      errorAlert(error, '', '');
-    })
-    })
-  })
-}
-
-function proxyAxios(data){
-  return new Promise((res)=>{
-    let axiosOpts = {
-
-    }
-    if (data.proxyUse == true){
-      let proxyHost = data.proxy.split(':')
-      axiosOpts.proxy = {
-        host: 'https://' + proxyHost[0], 
-        port: proxyHost[1],
-      }
-      
-    }
-    res(axiosOpts)
-  })
-}
-
-module.exports = down_mp4scrape;
-=======
 function scraping(data) {
-    var _this = this;
-    return new Promise(function (resolve) {
-        proxyAxios(data).then(function (axiosOpts) {
-            axios
-                .get(data.URL, axiosOpts)
-                .then(function (response, error) { return __awaiter(_this, void 0, void 0, function () {
-                var regex, videoURLS, tempURL;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            regex = /[0-9A-Za-z\/\.\-\:\_\\\-]+.\.mp4/g;
-                            videoURLS = response.data.match(regex);
-                            return [4 /*yield*/, urlFix(videoURLS[0])];
-                        case 1:
-                            tempURL = _a.sent();
-                            if (data.URL.includes('metro.co.uk')) {
-                                tempURL = tempURL.replace("640x360", "1024x576").replace("960x540", "1024x576").replace("480x270", "1024x576");
-                            }
-                            data.URL = tempURL;
-                            resolve(data);
-                            return [2 /*return*/];
-                    }
-                });
-            }); })["catch"](function (error) {
-                errorAlert(error, '', '');
+  var _this = this;
+  return new Promise(function (resolve) {
+    proxyAxios(data).then(function (axiosOpts) {
+      axios
+        .get(data.URL, axiosOpts)
+        .then(function (response, error) {
+          return __awaiter(_this, void 0, void 0, function () {
+            var regex, videoURLS, tempURL;
+            return __generator(this, function (_a) {
+              switch (_a.label) {
+                case 0:
+                  regex = /[0-9A-Za-z\/\.\-\:\_\\\-]+.\.mp4/g;
+                  videoURLS = response.data.match(regex);
+                  return [4 /*yield*/, urlFix(videoURLS[0])];
+                case 1:
+                  tempURL = _a.sent();
+                  if (data.URL.includes('metro.co.uk')) {
+                    tempURL = tempURL
+                      .replace('640x360', '1024x576')
+                      .replace('960x540', '1024x576')
+                      .replace('480x270', '1024x576');
+                  }
+                  data.URL = tempURL;
+                  resolve(data);
+                  return [2 /*return*/];
+              }
             });
+          });
+        })
+        ['catch'](function (error) {
+          errorAlert(error, '', '');
         });
     });
+  });
 }
 function proxyAxios(data) {
-    return new Promise(function (res) {
-        var axiosOpts = {};
-        if (data.proxyUse == true) {
-            var proxyHost = data.proxy.split(':');
-            console.log(proxyHost);
-            axiosOpts.proxy = {
-                host: 'https://' + proxyHost[0],
-                port: proxyHost[1]
-            };
-        }
-        res(axiosOpts);
-    });
+  return new Promise(function (res) {
+    var axiosOpts = {};
+    if (data.proxyUse == true) {
+      var proxyHost = data.proxy.split(':');
+      console.log(proxyHost);
+      axiosOpts.proxy = {
+        host: 'https://' + proxyHost[0],
+        port: proxyHost[1],
+      };
+    }
+    res(axiosOpts);
+  });
 }
 module.exports = down_mp4;
 //# sourceMappingURL=down_mp4.js.map
->>>>>>> Stashed changes:scripts/downloader/scrapers/down_mp4.js
