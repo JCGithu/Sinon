7b62dcde56b941953aab1bb49166c924ec0602be
jcgithu <jack.c.gracie@gmail.com>
2021-02-18 13:44:27 +0000

    removed instagram

--- scripts/components/optionsPage.js
+++ scripts/components/optionsPage.js
@@ -9,23 +9,19 @@ const optionsPage = {
 					<div class="optionscontainer">
 						<div class='optionscontainertwo'>
 							<a>Dark Mode</a>
-							<input class="container_toggle" type="checkbox" id="darkswitch" name="mode">
-							<label for="darkswitch">Toggle</label>
+							<input class="container_toggle" type="checkbox" id="darkMode" name="mode">
+							<label for="darkMode">Toggle</label>
 							<a style="margin-top: 10px;">Auto-delete URL on completion</a>
-							<input class="container_toggle" type="checkbox" id="urlswitch" name="urlwipe">
-							<label for="urlswitch">Toggle</label>
+							<input class="container_toggle" type="checkbox" id="urlWipe" name="urlwipe">
+							<label for="urlWipe">Toggle</label>
 							<a style="margin-top: 10px;">Country select</a>
-							<select id='geoFormat' class="geoFormat" classNamePrefix='conv'>
+							<select id='geo' class="geo" classNamePrefix='conv'>
 								<option value='UK'>UK</option>
 								<option value='US'>US</option>
 							</select>
 							<a style="margin-top: 10px;">Custom Proxy</a>
-							<input type="text" id="proxyInput" placeholder="Enter Custom Proxy" style='width: 60vw !important'>
+							<input type="text" id="customProxy" placeholder="Enter Custom Proxy" style='width: 60vw !important'>
 							<p>Proxies are only used if a website is <br> not available in your country</p>
-							<a>Instagram Username</a>
-							<input type="text" id="InstaUse" placeholder="Enter Instagram Username" style='width: 60vw !important'>
-							<a style="margin-top: 10px;">Instagram Password</a>
-							<input type="password" id="InstaPass" placeholder="Enter Instagram Password" style='width: 60vw !important'>
 							<button id="settingDelete" class="grow-on-hover">Delete settings</button>
 						</div>
 					</div>
