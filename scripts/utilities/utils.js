ac463b073b6ed26e022866733038805e49e1fb23
jcgithu <jack.c.gracie@gmail.com>
2021-02-18 13:48:02 +0000

    version checker patch

--- scripts/utilities/utils.js
+++ scripts/utilities/utils.js
@@ -12,8 +12,8 @@ function copyString(str) {
 }
 
 function swalColours() {
-  let darkSwitch = document.getElementById('darkswitch');
-  if (darkSwitch.checked) {
+  let darkMode = document.getElementById('darkMode');
+  if (darkMode.checked) {
     (swalColour.fail = '#232323'), (swalColour.loading = '#2c3e50'), (swalColour.pass = '#2c3e50');
   } else {
     (swalColour.fail = '#ED6A5A'), (swalColour.loading = 'rgba(0,0,0,0.4)'), (swalColour.pass = '#30bced');
@@ -30,7 +30,7 @@ function progressBar(progress, format, targetFiles) {
     progressText.textContent = progress.timemark.match('dd:dd:dd)');
   } else {
     if (format.indexOf('concat') >= 0) {
-      progress.percent = progress.percent / (targetFiles.length);
+      progress.percent = progress.percent / targetFiles.length;
     }
     if (progress.percent >= 100) {
       progress.percent = 99.9;
@@ -54,8 +54,7 @@ function docReady(fn) {
 }
 
 function URLwipe() {
-  let urlwipebox = document.querySelector('input[name=urlwipe]');
-  if (urlwipebox.checked == true) {
+  if (document.getElementById('urlWipe').checked) {
     console.log('URL Wipe applied');
     document.getElementById('inputURL').value = '';
   }
@@ -64,17 +63,16 @@ function URLwipe() {
 const axios = require('axios');
 
 async function versionChecker() {
-  return axios
-    .get('http://colloquial.studio/sinondata.json')
-    .then(async (response) => {
-      let element = document.getElementById('ver');
-      if (element.innerHTML !== response.data.version){
-        element.style = 'color:var(--hover)'
-        let docs = document.getElementById('documentation');
-        let docText = `<h4 style="color:var(--hover)">// Current version of Sinon is ${response.data.version}</h4>`+ docs.innerHTML;
-        docs.innerHTML = docText; 
-      }
-    });
+  return axios.get('http://colloquial.studio/sinondata.json').then(async (response) => {
+    let element = document.getElementById('ver');
+    if (element.innerHTML !== response.data.version) {
+      element.style = 'color:var(--hover)';
+      let docs = document.getElementById('documentation');
+      let docText =
+        `<h4 style="color:var(--hover)">// Current version of Sinon is ${response.data.version}</h4>` + docs.innerHTML;
+      docs.innerHTML = docText;
+    }
+  });
 }
 
-module.exports = { docReady, lineBreak, progressBar, swalColours, copyString, URLwipe, versionChecker};
+module.exports = { docReady, lineBreak, progressBar, swalColours, copyString, URLwipe, versionChecker };
